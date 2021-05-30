41595b746480602379b0303876fbe801dab6072a
Jason Cohen <jason.cohen@wpengine.com>
2021-05-30 09:54:09 -0500

    parsing some CSS formats

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 
+// Global regular expressions
+const RE_THREE_DIGIT = /#([0-9a-fA-F])([0-9a-fA-F])([0-9a-fA-F])([0-9a-fA-F])?\b/;
+const RE_SIX_DIGIT = /#([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})?\b/;
+const RE_RGB = /\brgb\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)(?:\s*,\s*(\d+))?\s*\)/;
+
 // Global functions for speed
 const round = Math.round
+
+// Parse string hex digits into a number
+function fromHex(s: string): number {
+  return parseInt(s, 16);
+}
 
 /**
  * Converts a number in `[0,1]` into a 2-digit hex string in the range `[0,255]`.  Clamps if outside that range.
  */
 function h2(x: number): string {
-  x = round(x * 256)
+  x = round(x * 255)
   if (x < 0) return '00'
   if (x < 16) return '0' + x.toString(16)
   if (x < 255) return x.toString(16)
@@ -21,6 +31,26 @@ export class Color {
    */
   constructor(private r: number, private g: number, private b: number, private a: number) {
     // nothing else to do
+  }
+
+  /**
+   * Reads a color from a CSS string, or returns `null` if the color is invalid.
+   * Supports 'transparent' but no other written colors.
+   */
+  static fromCss(s: string): Color {
+    let m: RegExpMatchArray | null;
+    if (s === "transparent") return new Color(0, 0, 0, 0);
+    if (m = s.match(RE_SIX_DIGIT)) {
+      return new Color(fromHex(m[1]) / 255, fromHex(m[2]) / 255, fromHex(m[3]) / 255, m[4] ? fromHex(m[4]) / 255 : 1)
+    }
+    if (m = s.match(RE_THREE_DIGIT)) {
+      return new Color(fromHex(m[1] + m[1]) / 255, fromHex(m[2] + m[2]) / 255, fromHex(m[3] + m[3]) / 255, m[4] ? fromHex(m[4] + m[4]) / 255 : 1)
+    }
+    if (m = s.match(RE_RGB)) {
+      return new Color(parseFloat(m[1]) / 255, parseFloat(m[2]) / 255, parseFloat(m[3]) / 255, m[4] ? parseFloat(m[4]) / 255 : 1)
+    }
+    // Don't know
+    return new Color(0, 0, 0, 0);
   }
 
   /**
--- test/index.test.ts
+++ test/index.test.ts
@@ -5,6 +5,69 @@ import { Color } from "../src";
 test("construction", () => {
   expect(new Color(0, 0, 0, 0).toCss()).toBe('#00000000');
   expect(new Color(1, 1, 1, 1).toCss()).toBe('#ffffffff');
-  expect(new Color(0.5, 0.75, 0.25, 0.0625).toCss()).toBe('#80c04010');
+  expect(new Color(0.5, 0.75, 0.25, 0.0625).toCss()).toBe('#80bf4010');
+  expect(new Color(0.502, 0.753, 0.25, 0.0625).toCss()).toBe('#80c04010');
   expect(new Color(-1, -0.001, 1.0001, 2).toCss()).toBe('#0000ffff');
+});
+
+test("invalid color", () => {
+  function t(p: string) { expect(Color.fromCss(p).toCss()).toBe('#00000000') }
+
+  t('')
+  t('tacos')
+  t('x')
+});
+
+test("named color", () => {
+  expect(Color.fromCss("transparent").toCss()).toBe('#00000000')
+});
+
+test("from #RGB(A)", () => {
+  function t(p: string, q: string) { expect(Color.fromCss(p).toCss()).toBe(q) }
+
+  // full string
+  t('#0000', '#00000000')
+  t('#ffff', '#ffffffff')
+  t('#1234', '#11223344')
+  t('#edbc', '#eeddbbcc')
+
+  // without alpha channel
+  t('#000', '#000000ff')
+  t('#fff', '#ffffffff')
+  t('#123', '#112233ff')
+  t('#edb', '#eeddbbff')
+});
+
+test("from #RRGGBB(AA)", () => {
+  function t(p: string, q: string) { expect(Color.fromCss(p).toCss()).toBe(q) }
+
+  // full string
+  t('#00000000', '#00000000')
+  t('#ffffffff', '#ffffffff')
+  t('#80c04010', '#80c04010')
+  t('#fe01c912', '#fe01c912')
+  t('#0000ffff', '#0000ffff')
+
+  // without alpha channel
+  t('#000000', '#000000ff')
+  t('#ffffff', '#ffffffff')
+  t('#80c040', '#80c040ff')
+  t('#fe01c9', '#fe01c9ff')
+  t('#0000ff', '#0000ffff')
+});
+
+test("from rgb(r,g,b(,a))", () => {
+  function t(r: number, g: number, b: number, a: number) {
+    const target = new Color(r / 255, g / 255, b / 255, a / 255).toCss();
+    const targetNoAlpha = new Color(r / 255, g / 255, b / 255, 1).toCss();
+
+    expect(Color.fromCss(`rgb(${r},${g},${b},${a})`).toCss()).toBe(target)
+    expect(Color.fromCss(`rgb(${r},${g},${b})`).toCss()).toBe(targetNoAlpha)
+  }
+
+  t(0, 0, 0, 0)
+  t(1, 2, 3, 4)
+  t(255, 255, 255, 255)
+  t(255, 254, 253, 252)
+  t(179, 180, 181, 182)
 });
